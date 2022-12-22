8130a23104db11d0f8c5128279dd9396fe680fad
RahuBirla <birlarahul340@gmail.com>
2022-12-22 13:50:59 +0530

    Project 1 good morning

--- demoreact/src/index.jsx
+++ demoreact/src/index.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.css"
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
-let fname = "Random";
-let lname = "Images";
 
-let url1 = "https://picsum.photos/200/200?grayscale";
-let url2 = "https://picsum.photos/200/200?whale";
-let url3 = "https://picsum.photos/200/200?cricket";
+let dt = new Date(2022, 12, 22, 19);
+let hour = dt.getHours();
 
-let img_border_style = { border: "2px solid red", borderRadius: "10px" };
+let greet;
+let color_change = { backgroundColor: "red" };
+
+if (hour <= 12) {
+    greet = "Good Morning"
+    color_change.backgroundColor = "red"
+} else if (hour > 12 && hour <= 18) {
+    greet = "Good Afternoon";
+    color_change.backgroundColor = "blue"
+} else {
+    greet = "Good Night"
+    color_change.backgroundColor = "yellow"
+}
 
 
 
 root.render(
     <>
-        <div className="container">
-            <h1>{`Hello This is ${fname} ${lname}`}</h1>
-            <div className="all_img">
-                <img src={url1} alt="random_img" />
-                <img src={url2} alt="random_img" />
-                <img src={url3} alt="random_img" />
-            </div>
-            <div className="all_img" style={img_border_style}>
-                <img src={url1} alt="random_img" />
-                <img src={url2} alt="random_img" />
-                <img src={url3} alt="random_img" />
-            </div>
+        <div className="container" style={color_change}>
+            <h1>Hello! {greet}</h1>
         </div>
     </>
 )
