fc1f074048cd190102fc2a141abb91868dc4ad2a
RahuBirla <birlarahul340@gmail.com>
2022-12-28 14:19:29 +0530

    Condition and click

---
+++ demoreact/src/Components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Button from "./Button";
+import Conditional from "./Conditional";
+
+const App = () => {
+
+
+    return (
+        <>
+            <div className="container">
+                <Button />
+                <Conditional />
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ demoreact/src/Components/Conditional.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import IsGoal from './IsGoal';
+
+const Conditional = () => {
+    
+    return (
+        <>
+            <IsGoal is="false"/>
+        </>
+    )
+}
+
+export default Conditional
