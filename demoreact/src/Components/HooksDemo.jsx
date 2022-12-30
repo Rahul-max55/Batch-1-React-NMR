d5dd2a5ab761f53ed3108be23b5ed1f3abac2eb6
RahuBirla <birlarahul340@gmail.com>
2022-12-30 14:14:13 +0530

    UseState hook is completed

--- demoreact/src/Components/HooksDemo.jsx
+++ demoreact/src/Components/HooksDemo.jsx
@@ -1,29 +1,48 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 
 const HooksDemo = () => {
 
 
 
-    let [val, setVal] = useState(1);
-    let [car, setCar] = useState("BMW");
+    let [val, setVal] = useState({
+        ritik: 1,
+        car: "BMW",
+        city: "Indore"
+    });
+    // console.log(val.ritik);
 
     const increment = () => {
-        setVal(val + 4);
+        let sum = val.ritik + 1;
+        setVal({
+            ...val,
+            ritik: sum
+        })
     }
 
-    const changeCar = () => {
-        car === "BMW" ? setCar("honda") : setCar("BMW");
+    const changeCar = (e) => {
+        let name = e.target.value;
+        name === "carButton" ? val.car === "BMW" ? setVal({ ...val, car: "Honda" }) : setVal({ ...val, car: "BMW" }) :
+            val.city === "indore" ? setVal({ ...val, city: "pune" }) : setVal({ ...val, city: "indore" });
     }
 
 
     return (
-
         <>
-            <h1>{val}</h1>
+            <h1>{val.ritik}</h1>
+            <h1>{val.car}</h1>
+            <h1>{val.city}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={changeCar} value="carButton" >Change car name</button>
+            <button onClick={changeCar}>Change City name</button>
+
+            {/* <h1>{val}</h1>
             <h1>{car}</h1>
-            <button onClick={increment} >+</button>
-            <button onClick={changeCar} >Change car name</button>
+            <h1>{city}</h1>
+            <button onClick={increment}>+</button> */}
+            {/* <button onClick={changeCar} value="carButton" >Change car name</button>
+            <button onClick={changeCar}>Change city name</button> */}
         </>
     )
 }
