976e8caa2ad3077bcf519c5a2cf44e87e4fa9693
RahuBirla <birlarahul340@gmail.com>
2023-01-13 15:05:51 +0530

    use Callback function is updated

--- demoreact/src/Components/App.jsx
+++ demoreact/src/Components/App.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
-import ComA from "./ComA";
-
-
+import Todo from "./Todo";
 
 const App = () => {
+
+    const [count, setCount] = useState(0);
+    const [todo, setTOdo] = useState(["old item"]);
+
+    const addItems = useCallback(() => {
+        return setTOdo([...todo, "newItem"]);
+    }, [todo])
+    // setTOdo((v) => {[...v, "newItem"]});
+    
+    // const addItems = () => {
+    //     setTOdo([...todo, "newItem"])
+    // }
+
     return (
-        <React.StrictMode>
-                <ComA />
-        </React.StrictMode>
+        <>
+            <h1>{count}</h1>
+            <button onClick={() => { setCount(count + 1) }} >start count</button>
+            <br />
+            <Todo addItems={addItems} todo={todo} />
+        </>
+
     )
 }
 
